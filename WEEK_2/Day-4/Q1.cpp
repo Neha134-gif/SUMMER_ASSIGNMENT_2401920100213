9ac5f7354587bcb169b3f15b2f4abb1eab0a1e80
Neha Mishra <nehamishra73667@gmail.com>
2026-06-15 15:34:53 +0530

    done

--- WEEK_2/Day-4/Q1.cpp
+++ WEEK_2/Day-4/Q1.cpp
@@ -1,2 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        int l = 0, r = 0;
+        while (r < n) {
+            if (s[r] == ' ') {
+                reverse(s.begin() + l, s.begin() + r);
+                l = r + 1;
+            }
+            r++;
+        }
+        reverse(s.begin() + l, s.end());
+        return s;
+    }
+};
--- WEEK_2/Day-4/Q2.cpp
+++ WEEK_2/Day-4/Q2.cpp
@@ -1,2 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ']') {
+                st.push(string(1, s[i]));
+            } else {
+                string temp = "";
+                while (st.top() != "[") {
+                    temp = st.top() + temp;
+                    st.pop();
+                }
+                st.pop(); // remove '['
+                string num = "";
+                while (!st.empty() && isdigit(st.top()[0])) {
+                    num = st.top() + num;
+                    st.pop();
+                }
+
+                int n = stoi(num);
+
+                string res = "";
+
+                for (int j = 0; j < n; j++) {
+                    res += temp;
+                }
+
+                st.push(res);
+            }
+        }
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
