77719321affbf5181332f70bfe38f2f34be85bdc
Neha Mishra <nehamishra73667@gmail.com>
2026-06-19 09:15:47 +0530

    uploaded

--- WEEK_3/Day-3/Q1.cpp
+++ WEEK_3/Day-3/Q1.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            } else {
+                if (st.empty())
+                    return false;
+                char ch = st.top();
+                st.pop();
+                if (s[i] == ')' && ch != '(' || s[i] == ']' && ch != '[' ||
+                    s[i] == '}' && ch != '{') {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
--- WEEK_3/Day-3/Q2.cpp
+++ WEEK_3/Day-3/Q2.cpp
@@ -1,2 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+class MinStack {
+public:
+    MinStack() {}
+    stack<pair<int, int>> st;
+
+    void push(int value) {
+        if (st.empty()) {
+            st.push({value, value});
+        } else {
+            int minVal = min(value, st.top().second);
+            st.push({value, minVal});
+        }
+    }
+
+    void pop() { st.pop(); }
+
+    int top() { return st.top().first; }
+
+    int getMin() { return st.top().second; }
+};
