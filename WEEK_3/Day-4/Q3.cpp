d72f05f181cc628567dbe4bb82df3f649098aabc
Neha Mishra <nehamishra73667@gmail.com>
2026-06-21 15:10:01 +0530

    uploaded

--- WEEK_3/Day-4/Q3.cpp
+++ WEEK_3/Day-4/Q3.cpp
@@ -1,2 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+
+        int n = heights.size();
+        stack<int> st;
+        int maxi = 0;
+
+        for (int i = 0; i <= n; i++) {
+
+            while (!st.empty() &&
+                  (i == n || heights[st.top()] >= heights[i])) {
+
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty()) {
+                    width = i;
+                }
+                else {
+                    width = i - st.top() - 1;
+                }
+
+                maxi = max(maxi, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxi;
+    }
+};
--- WEEK_3/Day-5/Q3.cpp
+++ WEEK_3/Day-5/Q3.cpp
@@ -1,2 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+
+        deque<int> dq;
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Window ke bahar wale indices remove karo
+            while (!dq.empty() && dq.front() <= i - k) {
+                dq.pop_front();
+            }
+
+            // Chhote elements remove karo
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            // First window ke baad answer store karo
+            if (i >= k - 1) {
+                ans.push_back(nums[dq.front()]);
+            }
+        }
+
+        return ans;
+    }
+};
